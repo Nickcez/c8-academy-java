042990da601b1b8e7804567e4bb5c9fa6c7f607e
Nicolas Césaire <n.cesaire@outlook.com>
2025-10-20 11:43:40 -0400

    13 added update

---
+++ src/main/java/junit_books/Book.java
@@ -0,0 +1,16 @@
+package junit_books;
+
+public class Book {
+	private String title;
+	private double price;
+	Book(String title, double price){
+		this.title = title;
+		this.price = price;
+		}
+	public String getTitle(){
+		return this.title;
+		}
+	public String getBookInfo(){
+		return this.getTitle() + "-" + this.price;
+		}
+	}
